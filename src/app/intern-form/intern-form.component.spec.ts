937ed9f33e24579414a4cb034b786d82c6bfc530
unknown <nuralikhoja@gmail.com>
2019-07-18 12:19:29 +0530

    added form component

---
+++ src/app/intern-form/intern-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InternFormComponent } from './intern-form.component';
+
+describe('InternFormComponent', () => {
+  let component: InternFormComponent;
+  let fixture: ComponentFixture<InternFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InternFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InternFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
