d80d19851b88489140a6fcada973753428be53b6
unknown <nuralikhoja@gmail.com>
2019-07-18 12:19:44 +0530

    added reactive form support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { InternFormComponent } from './intern-form/intern-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InternFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
