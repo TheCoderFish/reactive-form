b6123a3821393065a91f8a9077e3a15ba7ff13e1
unknown <nuralikhoja@gmail.com>
2019-07-18 17:26:01 +0530

    working form with validation

--- src/app/intern-form/intern-form.component.ts
+++ src/app/intern-form/intern-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-intern-form',
@@ -8,21 +8,85 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class InternFormComponent implements OnInit {
 
+
   internProfile: FormGroup = this.fb.group({
-    name: [''],
-    qualification: [''],
+    name: ['', Validators.required],
+    qualification: ['', Validators.required],
     experience: [false],
     experienceDetails: this.fb.group({
       company: [''],
       years: [null]
     }),
-    domain: ['']
+    domain: ['', Validators.required]
   });
+
+
+  domains = ['Angular', '.NET', 'Database', 'QA', 'Xamarin', 'ML'];
+
+  isSubmitted = false;
+
+  successMessage = 'Data submitted successfully';
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    console.log(this.internProfile.value);
   }
 
+  onSubmit() {
+    this.isSubmitted = true;
+  }
+
+  editMode() {
+    this.isSubmitted = false;
+  }
+
+  get name() {
+    return this.internProfile.get('name').value;
+  }
+
+  set name(input: string) {
+    this.internProfile.patchValue({ name: input });
+  }
+
+  get qualification() {
+    return this.internProfile.get('qualification').value;
+  }
+
+  set qualification(input: string) {
+    this.internProfile.patchValue({ qualification: input });
+  }
+
+  get experience() {
+    return this.internProfile.get('experience').value;
+  }
+
+  set experience(input: string) {
+    this.internProfile.patchValue({ experience: input });
+  }
+
+  get company() {
+    const expDetail = this.internProfile.get('experienceDetails').value;
+    return expDetail.company;
+
+  }
+
+  set company(input: string) {
+    this.internProfile.patchValue({ experienceDetails: { company: input } });
+  }
+
+  get years() {
+    const expDetail = this.internProfile.get('experienceDetails').value;
+    return expDetail.years > 0 ? expDetail.years : 0;
+  }
+
+  set years(input: string) {
+    this.internProfile.patchValue({ years: input });
+  }
+  get domain() {
+    return this.internProfile.get('domain').value;
+  }
+
+  set domain(input: string) {
+    this.internProfile.patchValue({ domain: input });
+  }
 }
